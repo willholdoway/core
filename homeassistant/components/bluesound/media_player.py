24f85b9c6cace4d4a135630b071c8156276a2ec8
willholdoway <william@holdoway.co.uk>
2021-05-20 15:55:56 +0100

    Revert "Add mute feature rather than volume-controlled pseudo-mute"
    This reverts commit 3a8a47928f872bee0672ee87fb1233b21f39c00b.

--- homeassistant/components/bluesound/media_player.py
+++ homeassistant/components/bluesound/media_player.py
@@ -220,7 +220,7 @@ class BluesoundPlayer(MediaPlayerEntity):
         self._last_status_update = None
         self._is_online = False
         self._retry_remove = None
-        self._muted = False
+        self._lastvol = None
         self._master = None
         self._is_master = False
         self._group_name = None
@@ -660,13 +660,10 @@ class BluesoundPlayer(MediaPlayerEntity):
     @property
     def is_volume_muted(self):
         """Boolean if volume is currently muted."""
-        mute = self._status.get("mute")
-        if self.is_grouped:
-            mute = self._sync_status.get("@mute")
-
-        if mute is not None:
-            mute = bool(int(mute))
-        return mute
+        volume = self.volume_level
+        if not volume:
+            return None
+        return 0 <= volume < 0.001
 
     @property
     def name(self):
@@ -1047,5 +1044,10 @@ class BluesoundPlayer(MediaPlayerEntity):
     async def async_mute_volume(self, mute):
         """Send mute command to media player."""
         if mute:
-            return await self.send_bluesound_command("Volume?mute=1")
-        return await self.send_bluesound_command("Volume?mute=0")
+            volume = self.volume_level
+            if volume > 0:
+                self._lastvol = volume
+            return await self.send_bluesound_command("Volume?level=0")
+        return await self.send_bluesound_command(
+            f"Volume?level={float(self._lastvol) * 100}"
+        )
